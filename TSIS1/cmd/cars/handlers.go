85bd1d8b49f3c9983f20e4b55302b1db20de7eec
Ermek <tolymbekov_ermek@mail.ru>
2024-02-04 18:53:53 +0600

    move: handlers from pkg/ to cmd/

---
+++ TSIS1/cmd/cars/handlers.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	cars "github.com/ermapula/golang-2024/pkg/cars"
+
+	"github.com/gorilla/mux"
+)
+
+type Response struct {
+	Cars []cars.Car `json:"cars"`
+}
+
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "500 Internal Server Error")
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok", "info": cars.Info()})
+}
+
+func Cars(w http.ResponseWriter, r *http.Request) {
+	cars := cars.GetCars()
+	respondWithJSON(w, http.StatusOK, cars)
+}
+
+func CarsOne(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	car, err := cars.GetCar(id)
+
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "404 Not Found")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, car)
+}
