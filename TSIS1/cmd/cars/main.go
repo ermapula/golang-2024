bcec0de39a85889faea1edb14a607ded08917d3b
Ermek <tolymbekov_ermek@mail.ru>
2024-02-04 19:03:50 +0600

    refactor: function calls in main.go

--- TSIS1/cmd/cars/main.go
+++ TSIS1/cmd/cars/main.go
@@ -4,17 +4,15 @@ import (
 	"fmt"
 	"net/http"
 
-	cars "github.com/ermapula/golang-2024/pkg/cars"
-
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/health-check", cars.HealthCheck).Methods("GET")
-	r.HandleFunc("/cars", cars.Cars).Methods("GET")
-	r.HandleFunc("/cars/{id}", cars.CarsOne).Methods("GET")
+	r.HandleFunc("/health-check", HealthCheck).Methods("GET")
+	r.HandleFunc("/cars", Cars).Methods("GET")
+	r.HandleFunc("/cars/{id}", CarsOne).Methods("GET")
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<p>/health-check - check api</p> <p>/cars - info of the race cars from Cars 2</p> <p>/cars/{id} - info of a specific car</p>")
